b77fc4f96eb9aaaa6b27b93c763a79d7f69216c4
Danielle Moore <danielle.moore0214@gmail.com>
2023-10-12 11:51:09 -0400

    modified todo App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [inputItem, setInputItem] = useState([]);
+  const [items, setItems] = useState([]);
+
+  const handleAddItem = () => {
+    const newItem = {id: items.length + 1, name: inputItem, isChecked: false};
+    setItems([...items, newItem]);
+  setInputItem('');
+  };
+
+  const handleCheck = (id) =>{
+    setItems(
+      items.map((item) => 
+      item.id === id ? {...item, isChecked: !item.isChecked} : item
+      )
+    )
+  };
+
+  const removeElement = (id) =>{
+    const newList = items.filter((item) => item.id !== id);
+    setItems(newList); 
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="List">
+        <h1>To Do List</h1>
+        <div className="add-tasks">
+          <label for="task">Add Task:</label>
+          <input type="text" id="task" value ={inputItem} onChange={(e) => setInputItem(e.target.value)}/>
+          <button type='submit'onClick={handleAddItem}>Submit</button>
+        </div>
+        <div className="to-do-list">
+          <h2 className="list-header">List</h2>
+          <ul>
+            {items.map((item) => (
+              <li key={item.id}>
+                <input type="checkbox" 
+                checked={item.isChecked}
+                onChange={() => handleCheck(item.id)} />
+                <span className={item.isChecked ? 'crossed' : ''}>{item.name}</span>
+                <button type='button' onClick={()=> removeElement(item.id)}>X</button>
+                </li>
+            )
+            )}
+            
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
+
 
 export default App;
